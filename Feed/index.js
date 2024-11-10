2bd0f9359fb0ca67b41850541f52340f6c84210a
Eliezer335 <jimmyeliezerdasilva@gmail.com>
2024-11-10 18:54:56 -0300

    🚧 progress: fazendo a funcao de criar novas publicacoes

--- Feed/index.js
+++ Feed/index.js
@@ -11,3 +11,23 @@ function fecharAba() {
     
 
 }
+
+function publicar() {
+    const todasPublicacaoes = document.querySelector('.publicacoes')
+    const txtLegenda = document.querySelector('.txtArea').value;
+    const imagemPublicacao = document.querySelector('.exibirImg') 
+    console.log('imagem publicacao',imagemPublicacao)
+    
+
+    todasPublicacaoes.innerHTML += `
+            <div class="publicacao">
+                <div class="containerUsuario">
+                    <div class="imgUsuario"></div>
+                    <div class="nomeUsuario">Eliezer Martinhago da silva</div>
+                </div>
+                <div class="legendaPublicacao">${txtLegenda}</div>
+                <div class="imgPublicacao">${imagemPublicacao}</div>
+            </div>
+    
+    `
+}
