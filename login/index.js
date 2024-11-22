c7bce27f7acbd473c084ee6f7e3fcd48816b2b6a
Eliezer335 <jimmyeliezerdasilva@gmail.com>
2024-11-21 22:25:37 -0300

    ♻️ Refactor: colocar carregamento do botao

--- login/index.js
+++ login/index.js
@@ -9,14 +9,22 @@ formulario.addEventListener('submit', (evento) => {
         email: email,
         password: senha
     };
+    const button = document.querySelector(".button");
+    console.log("botao",button)
 
     axios.post("https://socialcar-back.onrender.com/login", credenciais).then(response => {
+        button.disabled=true
+        button.textContent="Carregando..."
         console.log("resposta",response.data);
         const objetoString = JSON.stringify(response.data)
         localStorage.SocialCar = objetoString
-        alert("Login efetuado com Sucesso!")
+        window.location.href="../Feed/index.html#ancora"
     }).catch(error => {
+        button.disabled=true
+        button.textContent="Carregando..."
+        window.location.reload()
         console.error("Erro ao tentar fazer login", error);
-        alert("login não efetuado! Tente novamente mais tarde");
     })
+    button.disabled=false
+    button.textContent="Login"
 })
