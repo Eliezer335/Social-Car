09c7eefcdbe27f262e1224fc8257d3391517af04
gleiMoura <gle.bar.mou@gmail.com>
2024-11-25 15:37:01 -0300

    🐛 fix: resolvendo probelma com botão do login

--- login/index.js
+++ login/index.js
@@ -4,27 +4,26 @@ formulario.addEventListener('submit', (evento) => {
 
     const email = document.getElementById("email").value
     const senha = document.getElementById("senha").value
-    
+
     const credenciais = {
         email: email,
         password: senha
     };
     const button = document.querySelector(".button");
-    console.log("botao",button)
+
+    button.disabled = true;
+    button.textContent = "Carregando...";
 
     axios.post("https://socialcar-back.onrender.com/login", credenciais).then(response => {
-        button.disabled=true
-        button.textContent="Carregando..."
-        console.log("resposta",response.data);
+        console.log("resposta", response.data);
         const objetoString = JSON.stringify(response.data)
         localStorage.SocialCar = objetoString
-        window.location.href="../Feed/index.html#ancora"
+        window.location.href = "../Feed/index.html#ancora"
     }).catch(error => {
-        button.disabled=true
-        button.textContent="Carregando..."
         window.location.reload()
         console.error("Erro ao tentar fazer login", error);
+    }).finally(() => {
+        button.disabled = false;
+        button.textContent = 'login'
     })
-    button.disabled=false
-    button.textContent="Login"
 })
